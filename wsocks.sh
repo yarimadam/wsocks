153024ab909c5282226f69642192ed8c73456444
Tuncay Üner <morefox@gmail.com>
2016-08-14 00:52:57 +0300

    custom socks server support added

--- wsocks.sh
+++ wsocks.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 read -p "ssh user: " ssh_user
 read -p "ssh host: " ssh_host
+read -p "socks host: " socks_host
+read -p "socks port: " socks_port
 
 echo "connecting to $ssh_user@$ssh_host..."
-ssh ${ssh_user}@${ssh_host} -o "ProxyCommand=nc -X 5 -x 127.0.0.1:8527 %h %p"
+ssh ${ssh_user}@${ssh_host} -o "ProxyCommand=nc -X 5 -x ${socks_host}:${socks_port} %h %p"
